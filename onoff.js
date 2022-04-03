721d89f9e51abad3fcaeb4980aa8f89c6c61c1c3
Сергей Чернов <chernovsi@m2.ru>
2022-04-03 16:40:17 +0300

    add onoff

--- onoff.js
+++ onoff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import {Gpio} from 'onoff-debug';
-const pin2 = new Gpio(2, 'out');
+const pin2 = new Gpio(2, 'out', 'both', {reconfigureDirection: false});
 console.log(pin2.readSync());
 if(pin2.readSync() === 1){
     pin2.writeSync(0);
